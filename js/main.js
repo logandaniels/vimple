1a404a0ed4a42ef3419cd4a666bae90f9188f91d
Logan J Daniels <danie855@umn.edu>
2016-04-23 22:51:58 -0500

    Implement core tab manipulation (reload, prev/next tab, open tab, etc.)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
-window.addEventListener("keydown", function(event) {
-    switch (event.keyCode) {
-        case "J".charCodeAt(0):
-            window.scrollBy(0, 100);
-            break;
-        case "K".charCodeAt(0):
-            window.scrollBy(0, -100);
-            break;
-        case "H".charCodeAt(0):
-            if (event.shiftKey) {
-                console.log("Sending message");
-                safari.self.tab.dispatchMessage("prevTab", "test");
+var handleKeyPress = function(event) {
+        switch (event.keyCode) {
+            case "J".charCodeAt(0):
+                if (event.shiftKey) {
+                    window.history.back();
+                } else {
+                    window.scrollBy(0, 100);
+                }
+                break;
+            case "K".charCodeAt(0):
+                if (event.shiftKey) {
+                    window.history.forward();
+                } else {
+                    window.scrollBy(0, -100);
+                }
+                break;
+            case "L".charCodeAt(0):
+                if (event.shiftKey) {
+                    safari.self.tab.dispatchMessage("nextTab", "test");
+                }
+                break;
+            case "H".charCodeAt(0):
+                if (event.shiftKey) {
+                    safari.self.tab.dispatchMessage("prevTab", "test");
+                }
+                break;
+            case "T".charCodeAt(0):
+                safari.self.tab.dispatchMessage("newTab", "test");
+                break;
+            case "X".charCodeAt(0):
+                safari.self.tab.dispatchMessage("closeTab", "test");
+                break;
+            case "R".charCodeAt(0):
+                document.location.reload(true);
+                break;
             }
-    	}
-});
+        return false;
+}
 
+if (!document["hidden"]) {
+    document.addEventListener("keydown", handleKeyPress);
+}
+
+/* Slight delay before re-enabling keypress listeners when switching
+ * into a tab, to avoid repeated keypresses when switching tabs
+ */
+document.addEventListener("visibilitychange", function(event) {
+    if (!document["hidden"]) {
+        setTimeout(function() {
+            document.addEventListener("keydown", handleKeyPress);
+        }, 50);
+    } else {
+        document.removeEventListener("keydown", handleKeyPress);
+    }
+});
